4c5aa19397684e41f92bd665d61aa1d4b45db3d5
Anirudh GP <anirudh.gp@gmail.com>
2016-02-29 17:41:53 +0530

    made places public

--- app/src/main/java/com/example/blackops/custommap/MainActivity.java
+++ app/src/main/java/com/example/blackops/custommap/MainActivity.java
@@ -38,8 +38,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    String place;
-    Map m;
+    public static String place;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/  
+        });*/
         place = place.toLowerCase();
         String placearr[] = place.split(" ");
         place="";
