013785eb7f25dd32581a2fc77ca8bb156ae6a9ac
Anirudh GP <anirudh.gp@gmail.com>
2016-02-29 17:39:55 +0530

    Removed listview(2)

--- app/src/main/java/com/example/blackops/custommap/MainActivity.java
+++ app/src/main/java/com/example/blackops/custommap/MainActivity.java
@@ -53,8 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
-        place="Main Building";
+        });*/  
         place = place.toLowerCase();
         String placearr[] = place.split(" ");
         place="";
